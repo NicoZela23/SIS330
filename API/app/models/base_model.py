fix(model): precision status adjusted to handle new requests

API/app/models/base_model.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .utils import accuracy
+from models.utils import accuracy
 
 class ImageClassificationBase(nn.Module):
-    
     def training_step(self, batch):
         images, labels = batch
         out = self(images)

API/app/models/resnet9.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from .base_model import ImageClassificationBase
-from .utils import ConvBlock
+from models.base_model import ImageClassificationBase
+from models.utils import ConvBlock
 from config.config import MODEL_PATH
 
 class ResNet9(ImageClassificationBase):

API/app/services/prediction_service.py
@@ -1,8 +1,8 @@
 import torch
-from app.models import load_plant_disease_model
-from app.utils.image_utils import transform_image
+from models.resnet9 import load_plant_disease_model
+from utils.image_utils import transform_image
 from config.config import IN_CHANNELS, NUM_DISEASES, CLASS_NAMES
-from app.schemas.prediction import PredictionResult, PredictionResponse, parse_class_name
+from schemas.prediction import parse_class_name
 
 class PredictionService:
     def __init__(self):
@@ -11,23 +11,15 @@ class PredictionService:
     async def predict(self, file):
         image = await transform_image(file)
         with torch.no_grad():
-            outputs = self.model(image.unsqueeze(0))
-            probabilities = torch.nn.functional.softmax(outputs, dim=1)[0]
-            top_3_probs, top_3_indices = torch.topk(probabilities, 3)
-
-        top_3_predictions = []
-        for prob, idx in zip(top_3_probs, top_3_indices):
-            class_name = CLASS_NAMES[idx]
-            plant, condition = parse_class_name(class_name)
-            prediction = PredictionResult(
-                class_name=class_name,
-                plant=plant,
-                condition=condition,
-                confidence=prob.item()
-            )
-            top_3_predictions.append(prediction)
-
-        return PredictionResponse(
-            prediction=top_3_predictions[0],
-            top_3_predictions=top_3_predictions
-        )
+            xb = image.unsqueeze(0)
+            outputs = self.model(xb)
+            _, preds = torch.max(outputs, dim=1)
+        
+        class_name = CLASS_NAMES[preds[0].item()]
+        plant, condition = parse_class_name(class_name)
+        
+        return {
+            "predicted_plant": plant,
+            "condition": condition,
+            "confidence": torch.nn.functional.softmax(outputs, dim=1)[0][preds[0]].item()
+        }